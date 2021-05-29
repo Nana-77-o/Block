ece42d56e1b752f92918663fba54eb26e6e67b1a
Nana_77 <nana77amery@gmail.com>
2021-05-29 12:33:21 +0900

    Block

---
+++ Blok/Assets/Blok.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Blok : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ball")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+  
+}
---
+++ Blok/Assets/PlayerControrer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControrer : MonoBehaviour
+{
+    float accel = 1000.0f;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * accel, ForceMode.Impulse);
+
+    }
+}
